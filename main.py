2acef1dd09040e2b19b1220d756450d78f855b5c
Geovani Melo <geovanibarbosa@live.com>
2022-08-27 22:48:53 +0000

    teste final

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-import os
 import math
 import csv
-
 
 aux = ''
 soma_sl = 0
@@ -15,44 +12,38 @@ lines = []
 centroids = []
 dist_centroid = 0
 
-with open("iris.csv", newline = '') as file:
+
+with open("iris.csv", newline='') as file:
     reader = csv.reader(file)
     lines = list(reader)
 
-aux = str(lines[1]).split(',')[4].replace("'", '').replace(']', '').strip()
+lines.__delitem__(0)
+aux = lines[0][4]
 
-for i in range(1, len(lines)):
-  
-  line = str(lines[i]).strip().split(',')
-  
-  line = str(line).replace('[', '').replace(']', '').replace('"', '').replace("'", '').replace(' ','')
-  
-  
-  soma_sl+= float(str(line).split(',')[0])
-  soma_sw+= float(str(line).split(',')[1])
-  soma_pl+= float(str(line).split(',')[2])
-  soma_pw+= float(str(line).split(',')[3])
-  n+= 1
-  
-  if str(line).split(',')[4] != aux or i == len(lines)-1:
-   
-    centroids.append(
-      str(round(soma_sl / n, 3)) + ','  
-      + str(round(soma_sw / n, 3)) + ','
-      + str(round(soma_pl / n, 3)) + ','
-      + str(round(soma_pw / n, 3)) + ','
-      + str(aux))
+for i in range(len(lines)):
+    line = lines[i]
 
-    soma_sl = float(str(line).split(',')[0])
-    soma_sw = float(str(line).split(',')[1])
-    soma_pl = float(str(line).split(',')[2])
-    soma_pw = float(str(line).split(',')[3])
-    n = 1
-    
-    aux = str(line).split(',')[4].strip()
+    soma_sl += float(line[0])
+    soma_sw += float(line[1])
+    soma_pl += float(line[2])
+    soma_pw += float(line[3])
+    n += 1
 
+    if line[4] != aux or i == len(lines) - 1:
 
-#os.system('clear')
+        centroids.append(
+            str(round(soma_sl / n, 3)) + ',' + str(round(soma_sw / n, 3)) +
+            ',' + str(round(soma_pl / n, 3)) + ',' +
+            str(round(soma_pw / n, 3)) + ',' + aux)
+
+        soma_sl = float(line[0])
+        soma_sw = float(line[1])
+        soma_pl = float(line[2])
+        soma_pw = float(line[3])
+        n = 1
+
+        aux = str(line[4])
+
 print(centroids)
 print('\nNº de Centroides: ' + str(len(centroids)))
 print('\n')
@@ -63,9 +54,16 @@ point_sw_test = 3.4
 point_pl_test = 2.7
 point_pw_test = 4.3
 
-for j in range(0, len(centroids)):
-  line_centroid = str(centroids[j])
+for j in range(len(centroids)):
+    line_centroid = str(centroids[j]).split(',')
 
-  dist_centroid = math.sqrt((float(point_sl_test) - float(line_centroid.split(',')[0]))**2 + (float(point_sw_test) - float(line_centroid.split(',')[1]))**2 + (float(point_pl_test) - float(line_centroid.split(',')[2]))**2 + (float(point_pw_test) - float(line_centroid.split(',')[3]))**2)
+    dist_centroid = math.sqrt(
+        (point_sl_test - float(line_centroid[0]))**2 +
+        (point_sw_test - float(line_centroid[1]))**2 +
+        (point_pl_test - float(line_centroid[2]))**2 +
+        (point_pw_test - float(line_centroid[3]))**2
+    )
+
+    print("Distância do 'point_test' ao Centroide {} = {}".format(
+        j + 1, round(dist_centroid, 3)))
   
-  print("Distância do 'point_test' ao Centroide {} = {}".format(j+1, round(dist_centroid, 3)))
